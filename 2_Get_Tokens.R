02d9df4e7e8dcc7a2d24c673b7df36cc4957b9af
Nathan <99520642+E30895@users.noreply.github.com>
2024-04-30 16:48:40 -0300

    Update 2_Get_Tokens.R

--- 2_Get_Tokens.R
+++ 2_Get_Tokens.R
@@ -7,6 +7,24 @@ library(sentometrics)
 
 get_tokens = function(dataset) {
   
+  #' Obter Tokens
+  #'
+  #' Esta função extrai tokens (palavras) de um conjunto de dados fornecido.
+  #'
+  #' @param dataset O conjunto de dados contendo as informações de texto.
+  #' @return Um conjunto de dados contendo os tokens extraídos.
+  #' @examples
+  #' dataset <- data.frame(
+  #'   Texto = c("Esta é uma frase de exemplo.", "Outra frase aqui."),
+  #'   Traducao = c("This is an example sentence.", "Another sentence here.")
+  #' )
+  #' get_tokens(dataset)
+  #'
+  #' @importFrom dplyr mutate select
+  #' @importFrom tidyr unnest_tokens
+  #' @importFrom tidytext unnest_tokens
+  #' @export
+  
   tokens = dataset %>% 
     mutate(traducao = Traducao) %>% 
     select(-c('endereco', 'Texto', 'Num_Palavras-Chave')) %>% 
